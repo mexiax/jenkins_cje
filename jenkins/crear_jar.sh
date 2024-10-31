f114a2b2e0787e77865f029ef0244f5ca5da2ee9
mexiax <119631237+mexiax@users.noreply.github.com>
2024-10-31 12:07:33 +0100

    Update crear_jar.sh

--- jenkins/crear_jar.sh
+++ jenkins/crear_jar.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # Nombre del archivo y del paquete .jar
+PATH=${JENKINS_HOME}/jenkins/
 ARCHIVO="HolaMundo.java"
 CLASE="HolaMundo"
 JAR="hola_mundo.jar"
 
 # 1. Compilar el archivo Java
-javac $ARCHIVO
+javac ${PATH}${ARCHIVO}
 if [ $? -ne 0 ]; then
-    echo "Error al compilar $ARCHIVO"
+    echo "Error al compilar ${PATH}${ARCHIVO}"
     exit 1
 fi
 
@@ -16,7 +17,7 @@ fi
 echo "Main-Class: $CLASE" > MANIFEST.MF
 
 # 3. Crear el archivo .jar
-jar cvfm $JAR MANIFEST.MF $CLASE.class
+jar cvfm ${PATH}${JAR} MANIFEST.MF $CLASE.class
 
 # 4. Limpiar archivos intermedios
 rm MANIFEST.MF
